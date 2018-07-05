a4822c1bd3bb193c03f7ae73f8757f80e0b7b734
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-07-06 07:43:58 +0900

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
         children: ["/subject2/page1", "/subject2/page2", "/subject2/page3"]
       },
       {
-        title: "subject1",
+        title: "subject3",
         children: ["/subject3/page1", "/subject3/page2", "/subject3/page3"]
       }
     ]
