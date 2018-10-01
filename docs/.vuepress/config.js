a332a1e8120cb8f800742e4f1988d97290fc60f8
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-10-01 15:45:50 +0900

    modify indent

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,35 @@
 module.exports = {
-  title: "HelloWorld",
+  title: 'HelloWorld',
   themeConfig: {
     sidebar: [
       {
-        title: "index",
-        children: ["/"]
+        title: 'index',
+        children: ['/'],
       },
       {
-        title: "subject1",
-        children: ["/subject1/page1", "/subject1/page2", "/subject1/page3"]
+        title: 'subject1',
+        children: [
+          '/subject1/page1',
+          '/subject1/page2',
+          '/subject1/page3',
+        ],
       },
       {
-        title: "subject2",
-        children: ["/subject2/page1", "/subject2/page2", "/subject2/page3"]
+        title: 'subject2',
+        children: [
+          '/subject2/page1',
+          '/subject2/page2',
+          '/subject2/page3',
+        ],
       },
       {
-        title: "subject3",
-        children: ["/subject3/page1", "/subject3/page2", "/subject3/page3"]
-      }
-    ]
-  }
+        title: 'subject3',
+        children: [
+          '/subject3/page1',
+          '/subject3/page2',
+          '/subject3/page3',
+        ],
+      },
+    ],
+  },
 };
