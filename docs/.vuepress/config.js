dbc7d455e41c987d7219f3feac7e34f75b4a315b
ozaki <fs65ugumori@yahoo.co.jp>
2020-06-26 00:50:42 +0900

    add last updated plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const dayjs = require('dayjs');
+
 module.exports = {
   title: 'HelloWorld',
   themeConfig: {
@@ -9,27 +11,15 @@ module.exports = {
       },
       {
         title: 'Subject1',
-        children: [
-          '/subject1/page1',
-          '/subject1/page2',
-          '/subject1/page3',
-        ],
+        children: ['/subject1/page1', '/subject1/page2', '/subject1/page3'],
       },
       {
         title: 'Subject2',
-        children: [
-          '/subject2/page1',
-          '/subject2/page2',
-          '/subject2/page3',
-        ],
+        children: ['/subject2/page1', '/subject2/page2', '/subject2/page3'],
       },
       {
         title: 'Subject3',
-        children: [
-          '/subject3/page1',
-          '/subject3/page2',
-          '/subject3/page3',
-        ],
+        children: ['/subject3/page1', '/subject3/page2', '/subject3/page3'],
       },
     ],
   },
@@ -37,13 +27,18 @@ module.exports = {
     lineNumbers: true,
   },
   plugins: {
+    '@vuepress/last-updated': {
+      transformer: (timestamp, lang) => {
+        return dayjs(timestamp).format('YYYY/MM/DD');
+      },
+    },
     '@vuepress/back-to-top': {},
     '@vuepress/medium-zoom': {},
     '@vuepress/pwa': {
       serviceWorker: true,
       updatePopup: true,
     },
-    'seo': {},
+    seo: {},
   },
   head: [['link', { rel: 'manifest', href: '/manifest.json' }]],
 };
