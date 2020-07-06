cb7cb743aeea0a38212ccac7b883c56f3afb8fcc
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-07-06 18:40:23 +0900

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -40,7 +40,9 @@ module.exports = {
       serviceWorker: true,
       updatePopup: true,
     },
-    seo: {},
+    seo: {
+      description: () => 'ハンズオン資料',
+    },
   },
   head: [['link', { rel: 'manifest', href: '/manifest.json' }]],
 };
